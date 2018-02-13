d3b6277b53ac352416cabf06483e3a14e34d1b50
Michael Marango <mike.marango@gmail.com>
2018-02-13 10:46:54 -0800

    customized data models with attributes

--- ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
@@ -17,5 +17,16 @@ namespace ContosoUniversity.Data
         public DbSet<Course> Course { get; set; }
         public DbSet<Enrollment> Enrollment { get; set; }
         public DbSet<Student> Student { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Instructor> Instructor { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignment { get; set; }
+        public DbSet<CourseAssignment> CourseAssignment { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseID, c.InstructorID });
+        }
     }
 }
