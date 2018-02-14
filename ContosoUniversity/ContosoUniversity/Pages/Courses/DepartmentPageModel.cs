4e9858c953c91b9a97aeb452a1b4d59d10fd57a8
Michael Marango <mike.marango@gmail.com>
2018-02-14 03:20:30 -0800

    created base class to share common code

---
+++ ContosoUniversity/ContosoUniversity/Pages/Courses/DepartmentPageModel.cs
@@ -0,0 +1,26 @@
+﻿using ContosoUniversity.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class DepartmentPageModel : PageModel
+    {
+        public SelectList DepartmentList { get; set; }
+
+        public void PopulateDepartmentList(SchoolContext _context, object selectedDepartment = null)
+        {
+            var departments = from d in _context.Department
+                              orderby d.Name
+                              select d;
+
+            DepartmentList = new SelectList(departments.AsNoTracking(),
+                "DepartmentID", "Name", selectedDepartment);
+        }
+    }
+}
